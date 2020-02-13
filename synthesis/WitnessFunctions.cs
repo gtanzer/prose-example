ae33208d0d508e252fe977116ba65f34029ab539
Garrett Tanzer <gtanzer@college.harvard.edu>
2020-02-13 14:20:57 -0500

    rev-cond

--- synthesis/WitnessFunctions.cs
+++ synthesis/WitnessFunctions.cs
@@ -12,33 +12,34 @@ using Microsoft.ProgramSynthesis.Learning;
 namespace Ex {
     public class WitnessFunctions : DomainLearningLogic {
         public WitnessFunctions(Grammar grammar) : base(grammar) { }
-
-        [WitnessFunction(nameof(Semantics.Append), 0)]
-        public DisjunctiveExamplesSpec WitnessPrefix(GrammarRule rule, ExampleSpec spec) {
+        
+        [WitnessFunction(nameof(Semantics.Append), 1)]
+        public DisjunctiveExamplesSpec WitnessSuffix(GrammarRule rule, ExampleSpec spec) {
             var result = new Dictionary<State, IEnumerable<object>>();
             foreach (var example in spec.Examples) {
                 State inputState = example.Key;
                 var output = example.Value as string;
                 var substrings = new List<string>();
                 for (int i = 1; i <= output.Length; ++i) {
-                    substrings.Add(output.Substring(0, i));
+                    substrings.Add(output.Substring(i));
                 }
                 if (substrings.Count == 0) return null;
                 result[inputState] = substrings.Cast<object>();
-                Console.WriteLine("Prefix o: {0}\tp: {1}", output, String.Join(", ", substrings));
+                Console.WriteLine("Suffix o: {0}\tp: {1}", output, String.Join(", ", substrings));
             }
             return new DisjunctiveExamplesSpec(result);
         }
-
-        [WitnessFunction(nameof(Semantics.Append), 1, DependsOnParameters = new []{0})]
-        public ExampleSpec WitnessSuffix(GrammarRule rule, ExampleSpec spec, ExampleSpec prefixSpec) {
+        
+        [WitnessFunction(nameof(Semantics.Append), 0, DependsOnParameters = new []{1})]
+        public ExampleSpec WitnessPrefix(GrammarRule rule, ExampleSpec spec, ExampleSpec suffixSpec) {
             var result = new Dictionary<State, object>();
             foreach (var example in spec.Examples) {
                 State inputState = example.Key;
                 var output = example.Value as string;
-                var prefix = (string) prefixSpec.Examples[inputState];
-                result[inputState] = output.Substring(prefix.Length);
-                Console.WriteLine("Suffix o: {0}\tp: {1}\ts: {2}", output, prefix, output.Substring(prefix.Length));
+                var suffix = (string) suffixSpec.Examples[inputState];
+                var prefix = output.Substring(0, (output.Length-suffix.Length));
+                result[inputState] = prefix;
+                Console.WriteLine("Prefix o: {0}\tp: {1}\ts: {2}", output, prefix, suffix);
             }
             return new ExampleSpec(result);
         }
